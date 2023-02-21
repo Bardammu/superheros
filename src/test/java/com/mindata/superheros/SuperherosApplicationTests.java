ed38762b5cf4cc1efb88d8d7a1bada82d91e4d27
Juan Baca Paunero <wencesj@gmail.com>
2023-02-21 16:47:46 -0300

    Initial commit
    This commit adds the application structure and the initial Spring Boot
    dependencies.

---
+++ src/test/java/com/mindata/superheros/SuperherosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindata.superheros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
