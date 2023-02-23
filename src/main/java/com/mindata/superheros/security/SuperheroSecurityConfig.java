373a14481011d0a78679dcfdb2f5eca6c952c191
Juan Baca Paunero <wencesj@gmail.com>
2023-02-23 16:40:31 -0300

    Secure API with Basic Authentication
    This commit adds a first implementation of a security layer using Basic
    Authentication.

---
+++ src/main/java/com/mindata/superheros/security/SuperheroSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.mindata.superheros.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@Configuration
+public class SuperheroSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(GET, "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                ).httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
